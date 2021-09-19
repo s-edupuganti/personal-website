89eaa4de0200cb9d4785d3d35aea5782ae291e21
Sidharth <sedupuganti@tamu.edu>
2021-09-19 00:50:32 -0500

    Updated links again

--- main.js
+++ main.js
@@ -13,16 +13,17 @@ function closeNavBar() {
 function change() {
   document.body.classList.toggle("dark-mode");
 
-  if (document.body.classList.contains('dark-mode')) { //when the body has the class 'dark' currently
-    localStorage.setItem('darkMode', 'enabled'); //store this data if dark mode is on
-  } else {
-    localStorage.setItem('darkMode', 'disabled'); //store this data if dark mode is off
-  }
+  if(document.body.classList.contains('dark-mode')){ //when the body has the class 'dark' currently
+       localStorage.setItem('darkMode', 'enabled'); //store this data if dark mode is on
+   }else{
+       localStorage.setItem('darkMode', 'disabled'); //store this data if dark mode is off
+   }
 
 };
 
-if (localStorage.getItem('darkMode') == 'enabled') {
-  document.body.classList.toggle('dark-mode');
+if(localStorage.getItem('darkMode') == 'enabled'){
+   document.body.classList.toggle('dark-mode');
 };
+
 
 
--- toggle.js
+++ toggle.js
@@ -1,15 +1,16 @@
 function change() {
-   document.body.classList.toggle("light-mode");
-
-   if(document.body.classList.contains('light-mode')){ //when the body has the class 'dark' currently
-        localStorage.setItem('darkMode', 'enabled'); //store this data if dark mode is on
-    }else{
-        localStorage.setItem('darkMode', 'disabled'); //store this data if dark mode is off
-    }
-
-};
-
-if(localStorage.getItem('darkMode') == 'enabled'){
-    document.body.classList.toggle('light-mode');
-};
-
+    document.body.classList.toggle("dark-mode");
+ 
+    if(document.body.classList.contains('dark-mode')){ //when the body has the class 'dark' currently
+         localStorage.setItem('darkMode', 'enabled'); //store this data if dark mode is on
+     }else{
+         localStorage.setItem('darkMode', 'disabled'); //store this data if dark mode is off
+     }
+ 
+ };
+ 
+ if(localStorage.getItem('darkMode') == 'enabled'){
+     document.body.classList.toggle('dark-mode');
+ };
+ 
+ 
